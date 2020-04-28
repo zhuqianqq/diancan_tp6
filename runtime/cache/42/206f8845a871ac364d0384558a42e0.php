5e0c4dce8bab766aefe7a183840b39b3f3f65cbc
gaoshangguo@gmail.com <gaoshangguo@gmail.com>
2020-04-28 10:42:16 +0800

    更新gitignore

--- runtime/cache/42/206f8845a871ac364d0384558a42e0.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:1:{i:0;O:22:"think\annotation\Route":14:{s:6:"method";s:4:"POST";s:10:"middleware";N;s:3:"ext";N;s:8:"deny_ext";N;s:5:"https";N;s:6:"domain";N;s:14:"complete_match";N;s:5:"cache";N;s:4:"ajax";N;s:4:"pjax";N;s:4:"json";N;s:6:"filter";N;s:6:"append";N;s:5:"value";s:8:"getlists";}}
--- runtime/cache/83/00e5fcab137c3e60584752eb549583.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:2:{i:0;O:28:"think\annotation\route\Group":13:{s:10:"middleware";N;s:3:"ext";N;s:8:"deny_ext";N;s:5:"https";N;s:6:"domain";N;s:14:"complete_match";N;s:5:"cache";N;s:4:"ajax";N;s:4:"pjax";N;s:4:"json";N;s:6:"filter";N;s:6:"append";N;s:5:"value";s:11:"admin/rules";}i:1;O:33:"think\annotation\route\Middleware":1:{s:5:"value";a:1:{i:0;s:25:"app\middleware\CheckAdmin";}}}
