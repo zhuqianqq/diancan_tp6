81e1361c2c2b76803c121cde5fe6843d87fadf12
kevin <kevin@jiyw.com>
2020-04-30 12:01:27 +0800

    test

--- app/controller/api/Dingtalk.php
+++ app/controller/api/Dingtalk.php
@@ -4,6 +4,7 @@ declare (strict_types=1);
 namespace app\controller\api;
 require_once '../extend/dingtalk_isv_php_sdk/api/Auth.php';
 require_once '../extend/dingtalk_isv_php_sdk/api/ISVService.php';
+require_once '../extend/dingtalk_isv_php_sdk/api/User.php';
 
 use app\controller\api\Base;
 use think\annotation\route\Group;
@@ -79,12 +80,12 @@ class Dingtalk extends Base
         $suiteAccessToken = $this->ISVService->getSuiteAccessToken('10530003');
 
         $CorpInfo = json_decode($this->Auth->cache->getCorpInfo(),true);
-        dd($CorpInfo);
+
         foreach ($CorpInfo as $k => $v) {
            $CorpId = $k;
            $permanent_code = $v['permanent_code'];
         }
-        echo $CorpId . '=====';$permanent_code;die;
+        
         //获取企业授权凭证
         $isvCorpAccessToken = $this->ISVService->getIsvCorpAccessToken($suiteAccessToken,$CorpId,$permanent_code);
         //获取js_ticket
@@ -93,11 +94,15 @@ class Dingtalk extends Base
         dd($js_ticket);
     }
 
-
+    /**
+     * @Route("getUserInfo")
+     */
     public function getUserInfo()
     {
-        $user_info = $this->Auth->getUserInfo();
-        dd($user_info);
+       return json_ok(input('param.'));
+        // $User = new \User();
+        // $user_info = $User->getUserInfo();
+        // dd($user_info);
     }
 
 }
