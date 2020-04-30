916119989d2b160b46a02689f7bd39f6d2f90441
kevin <kevin@jiyw.com>
2020-04-30 10:40:56 +0800

    test

--- app/controller/api/Dingtalk.php
+++ app/controller/api/Dingtalk.php
@@ -77,7 +77,7 @@ class Dingtalk extends Base
         echo 'ISVService';
         //获取第三方应用凭证
         $suiteAccessToken = $this->ISVService->getSuiteAccessToken('10530003');
-
+        dd($suiteAccessToken);
         $CorpInfo = json_decode($this->Auth->cache->getCorpInfo(),true);
         foreach ($CorpInfo as $k => $v) {
            $CorpId = $k;
