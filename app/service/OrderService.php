f8d104f9bbe47d7ec6e94fd228602a2cebfe7fc2
ouyang <xibiao_ouyang@163.com>
2020-05-13 20:36:21 +0800

    fix

---
+++ app/service/OrderService.php
@@ -0,0 +1,76 @@
+<?php
+declare (strict_types=1);
+namespace app\service;
+
+use app\model\Eatery as E;
+use app\model\EateryRegister as ER;
+use app\model\Food;
+use app\MyException;
+use app\traits\ServiceTrait;
+use app\model\CompanyAdmin;
+use think\Db;
+use app\service\DingcanSysconfigService as SD;
+use app\model\Order as Ord;
+use app\model\OrderDetail as OrdD;
+use app\model\SysArea;
+use app\servicestaff\OrderService as SF;
+use app\service\EateryService as SE;
+
+/**
+ * 订单结算
+ * Class OrderService
+ * @package app\service
+ * @author  2066362155@qq.com
+ */
+class OrderService
+{
+
+    /**
+     * 订单结算列表
+     */
+    public static function lists()
+    {
+        $user_id = input('user_id', '', 'int');
+        $eatery_id = input('eatery_id', '', 'int');
+        $timeType = input('timeType', '', 'string');
+        if (!$user_id || !$eatery_id) {
+            throw new MyException(13001);
+        }
+        $eateryInfo = ER::find($eatery_id);
+        if (!$eateryInfo) {
+            throw new MyException(13002);
+        }
+        $userInfo = CompanyAdmin::getAdminInfoById($user_id);
+        if (!$userInfo) {
+            throw new MyException(13002);
+        }
+
+        //获取餐馆名称
+        $eateryName = SE::getNameById($eatery_id);
+        if (empty($eateryName)) {
+            throw new MyException(13002);
+        }
+        $where = ['company_id' => $userInfo->company_id, 'eatery_id' => $eatery_id];
+        //获取订单
+        $sql = 'select date_format(create_time, \'%Y-%m-%d\') dat, count(*) report_num,
+              sum(price) report_amount 
+                  from dc_order_detail
+                   where company_id=:company_id and eatery_id=:eatery_id 
+                   and create_time > :start_time and create_time <= :end_time group by date_format(create_time, \'%Y-%m-%d\');';
+
+        if (isset($timeType)) {
+            $timeInfo = getDateInfo($timeType);
+            print_r($timeInfo);die;
+            $where['start_time'] = $timeInfo['start_time'];
+            $where['end_time'] = $timeInfo['end_time'];
+        }
+        $res = \think\facade\Db::query($sql, $where);
+        echo \think\facade\Db::getlastsql();die;
+        foreach ($res as $k => $v) {
+            $res[$k]['eatery_name'] = $eateryName;
+        }
+
+        return $res;
+    }
+
+}
