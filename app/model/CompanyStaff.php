80ede4843a1e6424da55aa7ac951573e790097e7
ouyang <xibiao_ouyang@163.com>
2020-05-28 16:23:04 +0800

    fix

--- app/model/CompanyStaff.php
+++ app/model/CompanyStaff.php
@@ -56,7 +56,7 @@ class CompanyStaff extends BaseModel
         $result = \think\facade\Db::table($staffTable)
             ->alias('s')
             ->join(CompanyRegister::$_table . ' r', 'r.company_id = s.company_id')
-            ->join(DTDepartment::$_table . ' d', 'd.company_id = s.company_id and d.platform_departid = s.department_id')
+            ->join(DTDepartment::$_table . ' d', 'd.company_id = s.company_id and d.id = s.department_id')
             ->where('staff_status = 1 and staffid=:staffid',['staffid' => $user_id])
             ->field(['staffid','s.company_id','staff_name','d.id department_id','company_name','platform_departid','dept_name'])
             ->find();
