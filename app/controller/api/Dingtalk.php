88e5fa5858ec1a3bd07a70a57b6604c7f2cf8327
ouyang <xibiao_ouyang@163.com>
2020-06-01 17:53:01 +0800

    首页获取钉钉接口数据从redis中取

--- app/controller/api/Dingtalk.php
+++ app/controller/api/Dingtalk.php
@@ -146,7 +146,6 @@ class Dingtalk extends Base
     //isv应用免登陆的公司AccessToken
     public function getIsvCorpAccessToken($corpId)
     {
-
         //授权方企业ID
         $authCorpId = CORP_ID;
         //获取票据信息
@@ -156,9 +155,20 @@ class Dingtalk extends Base
 
         $ticketDatArr = \GuzzleHttp\json_decode($ticketData['biz_data'], true);
         $authDataArr = \GuzzleHttp\json_decode($authData['biz_data'], true);
-        $suiteAccessToken = $this->getSuiteAccessToken($ticketDatArr['suiteTicket']);
 
-        $isvCorpAccessToken = $this->ISVService->getIsvCorpAccessToken($suiteAccessToken, $corpId, $authDataArr['permanent_code']);
+        $suiteKey = 'suiteTicket' . $authCorpId;
+        if (Cache::get($suiteKey)) {
+            $suiteAccessToken = Cache::get($suiteKey);
+        } else {
+            $suiteAccessToken = $this->getSuiteAccessToken($ticketDatArr['suiteTicket']);
+        }
+
+        $accessTokenkey = "IsvCorpAccessToken_".$authCorpId;
+        if (Cache::get($accessTokenkey)) {
+            $isvCorpAccessToken = Cache::get($accessTokenkey);
+        } else {
+            $isvCorpAccessToken = $this->ISVService->getIsvCorpAccessToken($suiteAccessToken, $corpId, $authDataArr['permanent_code']);
+        }
 
         $key = 'corpAuthInfo_'.$corpId;
         if (!Cache::get($key)) {
