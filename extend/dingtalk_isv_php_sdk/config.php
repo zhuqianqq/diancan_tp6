e740e293e2326828b5b22ac0d9fa757b00fb6da8
kevin <kevin@jiyw.com>
2020-06-01 10:10:11 +0800

    增加测试config

--- extend/dingtalk_isv_php_sdk/config.php
+++ extend/dingtalk_isv_php_sdk/config.php
@@ -3,8 +3,10 @@ define('DIR_ROOT', dirname(__FILE__).'/');
 define("OAPI_HOST", "https://oapi.dingtalk.com");
 //Suite
 define("CORP_ID", "ding076b713cc1eff17735c2f4657eb6378f");
-define("SUITE_KEY", "suiteqoemhn2iyzgy9qql");
-define("SUITE_SECRET", "xdO3u7cUeX8nFOa99l5C6ekpZ0bZV4BHg31SFTU5sfVepiUi9ZnwRpcGFnAuGUiQ");
+define("SUITE_KEY", "suiteaquwgemwiirltuz8");//测试
+// define("SUITE_KEY", "suiteqoemhn2iyzgy9qql"); //正式应用
+define("SUITE_SECRET", "Q6QBsblURxUcFm9iBliZ8l8ikE3_WcChQL3Xt96D2gT__RgZ12vy5ZM5w1MeZt7j");//测试
+// define("SUITE_SECRET", "xdO3u7cUeX8nFOa99l5C6ekpZ0bZV4BHg31SFTU5sfVepiUi9ZnwRpcGFnAuGUiQ");//正式应用
 define("TOKEN", "diancantoken");
 define("APPID", "45800");
 define("ENCODING_AES_KEY", "ps9z6ftov7st9ljuv4szoq1obkuwybgnozh39zoonz6");
@@ -16,3 +18,8 @@ define("ENCODING_AES_KEY", "ps9z6ftov7st9ljuv4szoq1obkuwybgnozh39zoonz6");
 //define("TOKEN", "diancantoken");
 // define("APPID", "46153");
 //define("ENCODING_AES_KEY", "ps9z6ftov7st9ljuv4szoq1obkuwybgnozh39zoonz6");
+
+
+
+
+
